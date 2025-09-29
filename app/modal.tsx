0892b2c95b6a09b04d332222f928c4cb91806964
Christine B <(none)>
2025-09-29 10:23:03 +0200

    added helå

--- app/modal.tsx
+++ app/modal.tsx
@@ -10,6 +10,7 @@ export default function ModalScreen() {
       <ThemedText type="title">This is a modal</ThemedText>
       <Link href="/" dismissTo style={styles.link}>
         <ThemedText type="link">Go to home screen</ThemedText>
+        <ThemedText type="link">Helå</ThemedText>
       </Link>
     </ThemedView>
   );
